5b6ede0782251e4498217b7070c23e1412c480a9
S-a-b-r <po_fasty_grisha@mail.ru>
2024-12-23 01:54:09 +0400

    add tests

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -15,16 +15,27 @@ type TestStructLogin struct {
 
 func TestHandleCompressionLogin(t *testing.T) {
 
-	data1, err := hex.DecodeString("FF1B00780153F65136D233B0CECC4DCDB4F673B476B4343602002FF404E6")
-	if err != nil {
-		t.Fatal(err)
-	}
+	data1, _ := hex.DecodeString("FF1B00780153F65136D233B0CECC4DCDB4F673B476B4343602002FF404E6")
+	data2, _ := hex.DecodeString("ff1b00789c52f65136d233b03606036b3f476b474b632340000000ffff2f310474")
+	data3, _ := hex.DecodeString("ff1b00789c52f65136d233b036b2b4303537313331373533b1f673b476b4343602040000ffff4e1d05c5")
 
 	tests := []TestStructLogin{
 		{
-			name:    "Test valid all data",
+			name:    "Test valid all data 1",
 			buf:     *bytes.NewBuffer(data1),
 			want:    "imei",
+			wantErr: false,
+		},
+		{
+			name:    "Test valid all data 2",
+			buf:     *bytes.NewBuffer(data2),
+			want:    "333333",
+			wantErr: false,
+		},
+		{
+			name:    "Test valid all data 3",
+			buf:     *bytes.NewBuffer(data3),
+			want:    "298574647564",
 			wantErr: false,
 		},
 	}
@@ -34,16 +45,14 @@ func TestHandleCompressionLogin(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			res, err := p.HandleCompressionLogin(tt.buf)
 			if tt.wantErr == true && err != nil {
-				return
+				t.Fatal("ParseHandleCompressionLogin() should return error")
 			}
 
 			if (err != nil) != tt.wantErr {
-				t.Errorf("ParseHandleCompressionLogin() error = %v, wantErr %v", err, tt.wantErr)
-				return
+				t.Fatalf("ParseHandleCompressionLogin() error = %v, wantErr %v", err, tt.wantErr)
 			}
 			if res != tt.want {
-				t.Errorf("ParseHandleCompressionLogin() res = %s, want %s", res, tt.want)
-				return
+				t.Fatalf("ParseHandleCompressionLogin() res = %s, want %s", res, tt.want)
 			}
 			// if !reflect.DeepEqual(res, tt.want) {
 			// 	t.Errorf("StringToStruct() = %+v, %T, want %+v, %T", tt.itf, tt.itf, tt.want, tt.want)
